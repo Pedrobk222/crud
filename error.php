91b03f34efd72604cd3ab4432ff95302e592c402
Pedrobk222 <pedro.bortolucci1.ph@gmail.com>
2024-04-23 18:03:20 -0300

    Atualizando crud

---
+++ error.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <title>Erro</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        .wrapper {
+            width: 600px;
+            margin: 0 auto;
+        }
+        .header-logo {
+            text-align: center;
+            margin-bottom: 20px;
+        }
+        .header-logo img {
+            max-height: 100px;
+        }
+    </style>
+</head>
+<body>
+    <div class="header-logo">
+        <img src="img/logo.png" alt="Logotipo da DEVOPS Software House">
+    </div>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="mt-5 mb-3">Requisição Inválida</h2>
+                    <div class="alert alert-danger">Desculpe, você fez uma requisição inválida. Por favor <a href="index.php" class="alert-link">volte</a> e tente novamente.</div>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
---
+++ read.php
@@ -0,0 +1,100 @@
+<?php
+// Check existence of id parameter before processing further
+if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
+    // Include config file
+    require_once "config.php";
+    
+    // Prepare a select statement
+    $sql = "SELECT * FROM employees WHERE id = ?";
+    
+    if($stmt = mysqli_prepare($link, $sql)){
+        // Bind variables to the prepared statement as parameters
+        mysqli_stmt_bind_param($stmt, "i", $param_id);
+        
+        // Set parameters
+        $param_id = trim($_GET["id"]);
+        
+        // Attempt to execute the prepared statement
+        if(mysqli_stmt_execute($stmt)){
+            $result = mysqli_stmt_get_result($stmt);
+    
+            if(mysqli_num_rows($result) == 1){
+                /* Fetch result row as an associative array. Since the result set
+                contains only one row, we don't need to use while loop */
+                $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+                
+                // Retrieve individual field value
+                $name = $row["name"];
+                $address = $row["address"];
+                $salary = $row["salary"];
+            } else{
+                // URL doesn't contain valid id parameter. Redirect to error page
+                header("location: error.php");
+                exit();
+            }
+            
+        } else{
+            echo "Opa! Algo deu errado. Por favor, tente novamente mais tarde.";
+        }
+    }
+     
+    // Close statement
+    mysqli_stmt_close($stmt);
+    
+    // Close connection
+    mysqli_close($link);
+} else{
+    // URL doesn't contain id parameter. Redirect to error page
+    header("location: error.php");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <title>Visualizar Registro</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        .wrapper{
+            width: 600px;
+            margin: 0 auto;
+        }
+        .header-logo {
+            text-align: center;
+            margin-bottom: 20px;
+        }
+        .header-logo img {
+            max-height: 100px;
+        }
+    </style>
+</head>
+<body>
+    <div class="header-logo">
+        <img src="img/logo.png" alt="Logotipo da DEVOPS Software House">
+    </div>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1 class="mt-5 mb-3">Visualizar Registro</h1>
+                    <div class="form-group">
+                        <label>Nome</label>
+                        <p><b><?php echo $name; ?></b></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Endereço</label>
+                        <p><b><?php echo $address; ?></b></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Salário</label>
+                        <p><b><?php echo $salary; ?></b></p>
+                    </div>
+                    <p><a href="index.php" class="btn btn-primary">Voltar</a></p>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
