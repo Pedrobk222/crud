6d3c234ba610b73add8b681df81650c81d2a8e01
Pedro Henrique Bortolucci  Klayn <74112579+Pedrobk222@users.noreply.github.com>
2024-04-24 10:48:40 -0300

    Update employees.sql

--- employees.sql
+++ employees.sql
@@ -39,9 +39,9 @@ CREATE TABLE IF NOT EXISTS `employees` (
 --
 
 INSERT INTO `employees` (`id`, `name`, `address`, `salary`) VALUES
-(1, 'Roland Mendel', 'C/ Araquil, 67, Madrid', 5000),
-(2, 'Victoria Ashworth', '35 King George, London', 6500),
-(3, 'Martin Blank', '25, Rue Lauriston, Paris', 8000);
+(1, 'Luis', 'Rua 77, Araruna', 5000),
+(2, 'Ana', 'Rua 12, Campo Mourão', 16500),
+(3, 'Lucas', 'Rua 25, Campo Mourão', 2000);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
